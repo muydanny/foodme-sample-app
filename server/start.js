219e6f9d87e84959c92bf6042690101d81c33876
Danny Ramos <danramos21@gmail.com>
2022-08-03 11:39:46 -0700

    make it better

--- server/start.js
+++ server/start.js
@@ -1,5 +1,22 @@
+require('newrelic');
 var PORT = process.env.PORT || 3000;
 var STATIC_DIR = __dirname + '/../build';
 var DATA_FILE = __dirname + '/data/restaurants.json';
 
 require('./index').start(PORT, STATIC_DIR, DATA_FILE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
